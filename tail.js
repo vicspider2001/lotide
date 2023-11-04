d51c3c45da334d5b5e3ab757c2c3bbd32ee9f6e6
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-04 17:31:36 -0400

    Completed

---
+++ tail.js
@@ -0,0 +1,31 @@
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  } else {
+    return array.slice(1);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertArraysEqual(result, ["Lighthouse", "Labs"]);
