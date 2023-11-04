d8b4f2bf7ae10c1b2d2ff36230fe9fafe648a16d
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-04 14:39:21 -0400

    Completed

---
+++ head.js
@@ -0,0 +1,22 @@
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+}
+
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
