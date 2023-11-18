6cc78b1f0840f02a641517862de435edcb6ff5a3
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-18 13:04:46 -0500

    Completed Assignment

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
