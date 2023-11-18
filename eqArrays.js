f32a3f3407b1a8ce2f5e1038c900a9dbaac78e21
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-18 13:14:59 -0500

    updated with more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -23,3 +23,5 @@ const assertEqual = function(actual, expected) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), false);
 assertEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
+assertEqual(eqArrays([""], []), false);
+assertEqual(eqArrays([], []), true);
