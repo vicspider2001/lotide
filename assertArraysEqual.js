5622dfc46dcb73eb13096f84091515df4f9ee74f
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-18 16:25:48 -0500

    AssignmentCompleted

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 3, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], [1, 2, 3]);
+assertArraysEqual([""], []);
+assertArraysEqual([], []);
