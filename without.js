01f10cced0b2df0d8867fbb86734c3ad87355478
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-19 02:34:30 -0500

    Updated test cases

--- without.js
+++ without.js
@@ -43,4 +43,6 @@ const assertArraysEqual = function(actual, expected) {
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); 
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
