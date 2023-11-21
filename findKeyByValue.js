627bf4559876f3f1bd2b052fb1f239c0cef069a8
Victor Anogwih <anogwihvictor@gmail.com>
2023-11-21 13:38:34 -0500

    Assignment Completed

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// create a function called findKeyByValue
+// the function should take in a object and a value
+// function should scan object and return the first key which contains the value
+// function should return undefined if no key is found
+
+const findKeyByValue = function(shows, value) { // function is created
+  const keys = Object.keys(shows); // get Object keys of the object called shows
+  for (const key of keys) { // loop through the object keys
+    if (shows[key] === value) { // Condition the scans through the object and returns the first key
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const movies = {
+  action: "Top Boys",
+  Comedy: "Grace Land",
+  Suspense: " Wonderland"
+};
+console.log(findKeyByValue(movies, 'Top Boys'));
+
+assertEqual(findKeyByValue(movies, "Grace Land"), "Comedy");
+assertEqual(findKeyByValue(movies, "Top Boys"), "action");
+assertEqual(findKeyByValue(movies, "Anaconda"), "action");
+
+
