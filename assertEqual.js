f54bd2a6494443f6f02f2bf9fdfd65685449e850
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-06 15:12:10 -0500

    Refactored

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual("hello", "hello"));
-console.log(assertEqual("hello", "world"));
-console.log(assertEqual(42, 42));
-console.log(assertEqual(42, 7));
+
+module.exports = assertEqual;
