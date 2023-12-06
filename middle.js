77037ff49ecfcd83b2487685d2b5961b56305b75
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-06 18:08:24 -0500

    middleRefactored

--- middle.js
+++ middle.js
@@ -4,28 +4,6 @@
 // Use assertArrasEqual function to test middle function
 // For arrays with one or two elements, there is no middle. Return an empty array
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(arr) { // creates a function called middle that takes one array
   if (!Array.isArray(arr)) {
     return "Input is not an array"; // if input is not an array
@@ -45,18 +23,5 @@ const middle = function(arr) { // creates a function called middle that takes on
   
 };
 
-
-
-const testArray = [1, 2, 3];
-const testedArray = middle(testArray);
-assertArraysEqual(testedArray, [2]);
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-console.log(middle(['a', 'b', 'c']));
-console.log(middle(['a', 'b', 'c', 'e']));
-console.log(middle(1, 2, 3, 4, 5));
-console.log(middle('a', 'd', 'c', 'd', 'e'));
+module.exports = middle;
 
