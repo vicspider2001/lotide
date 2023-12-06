12480afae7cb7fef5a453c33514446f500711153
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-06 15:21:23 -0500

    test

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,8 @@
 const assertEqual = require('../assertEqual');
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual("hello", "hello"));
-console.log(assertEqual("hello", "world"));
-console.log(assertEqual(42, 42));
-console.log(assertEqual(42, 7));
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("hello", "world");
+assertEqual(42, 42);
+assertEqual(42, 7);
