2a660ac159c2a37778a4d82da16aba89a5785991
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-08 14:08:54 -0500

    newIndex

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
@@ -12,7 +12,6 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -34,7 +33,6 @@ module.exports = {
   findKeyByValue : findKeyByValue,
   flatten : flatten,
   letterPositions : letterPositions,
-  map : map,
   takeUntil : takeUntil,
   without : without
 
